0e528cfcd09dc546519a760151abbd92f7fb895d
Shaun Simpson <shaungsimpson@gmail.com>
2023-11-07 10:29:47 +1100

    Add placeholder for missing poster images. Add simple pagination

--- src/components/MovieResult.vue
+++ src/components/MovieResult.vue
@@ -3,9 +3,9 @@ defineProps(['movie'])
 </script>
 
 <template>
-    <div class="rounded-lg shadow border h-full border-gray-300 dark:border-gray-950 bg-gray-100 dark:bg-slate-700 transition hover:scale-105">
+    <div class="h-full transition bg-gray-100 border border-gray-300 rounded-lg shadow dark:border-gray-950 dark:bg-slate-700 hover:scale-105">
         <div class="w-full h-64 rounded-t-lg">
-            <img class="w-full h-full object-cover object-center rounded-t-lg" :src="movie.Poster" alt="" />
+            <img class="object-cover object-center w-full h-full rounded-t-lg" :src="movie.Poster == 'N/A' ? 'src/assets/image-not-available.png' : movie.Poster" alt="" />
         </div>
         <h3 class="px-3 py-4">{{ movie.Title }} ({{ movie.Year }})</h3>
     </div>
--- src/views/MoviesView.vue
+++ src/views/MoviesView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import MovieResult from '../components/MovieResult.vue'
 import { MagnifyingGlassIcon } from '@heroicons/vue/20/solid'
 
@@ -7,34 +7,45 @@ const apiKey = import.meta.env.VITE_API_KEY
 const search = ref('')
 const movies = ref([])
 const totalResults = ref(0)
+const currentPage = ref(1)
 
 const searchMovies = () => {
-    fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=${apiKey}`)
+    const page = currentPage.value ? `&page=${currentPage.value}` : ''
+    fetch(`http://www.omdbapi.com/?s=${search.value}&apikey=${apiKey}${page}`)
         .then((response) => response.json())
         .then((data) => {
             movies.value = data.Search
             totalResults.value = data.totalResults
         })
 }
+const fetchPage = (page) => {
+    currentPage.value = page
+    searchMovies()
+}
+
+const resultPages = computed(() => {
+    return Math.ceil(totalResults.value / 10) ?? 1
+})
+
 </script>
 
 <template>
     <main>
-        <div class="inline-flex items-center w-full space-x-6 justify-between">
+        <div class="inline-flex items-center justify-between w-full space-x-6">
             <h1>Movies</h1>
-            <div class="w-full ml-auto max-w-lg lg:max-w-xs" v-if="apiKey">
+            <div class="w-full max-w-lg ml-auto lg:max-w-xs" v-if="apiKey">
                 <label for="search" class="sr-only">Search</label>
                 <div class="relative">
                     <div
-                        class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
                     >
-                        <MagnifyingGlassIcon class="h-5 w-5 text-gray-400 dark:text-emerald-200" aria-hidden="true" />
+                        <MagnifyingGlassIcon class="w-5 h-5 text-gray-400 dark:text-emerald-200" aria-hidden="true" />
                     </div>
                     <input
                         id="search"
                         v-model="search"
                         name="search"
-                        class="block w-full rounded-md  dark:placeholder:text-emerald-200 placeholder:text-gray-400 dark:bg-gray-700 py-1.5 pl-10 pr-3 dark:text-gray-300 focus:bg-white focus:text-gray-900 dark:focus:text-gray-300 focus:ring-0 sm:text-sm sm:leading-6 border-gray-300 border dark:border-gray-700"
+                        class="block w-full rounded-md  dark:placeholder:text-emerald-200 placeholder:text-gray-400 dark:bg-gray-700 py-1.5 pl-10 pr-3 dark:text-gray-300 focus:bg-white focus:text-gray-900 dark:focus:text-gray-300 focus:ring-emerald-700 sm:text-sm sm:leading-6 border-gray-300 border dark:border-gray-700"
                         placeholder="Search"
                         type="search"
                         @change="searchMovies"
@@ -46,10 +57,22 @@ const searchMovies = () => {
             </div>
         </div>
         <div class="py-6 my-6">
-            <p class="text-center">{{ totalResults }} results found.</p>
+            <p class="text-center"><span v-if="resultPages > 1">Page {{ currentPage }} of {{ resultPages }}</span> ({{ totalResults }} results found.)</p>
             <div class="grid grid-cols-5 gap-3 py-5">
                 <MovieResult v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
             </div>
+        </div>
+        <div class="flex justify-center space-x-2" v-if="resultPages > 1">
+            <button
+                class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
+                :disabled="currentPage === 1"
+                @click="fetchPage(currentPage - 1)"
+            >Previous</button>
+            <button
+                class="px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
+                :disabled="currentPage === resultPages"
+                @click="fetchPage(currentPage + 1)"
+            >Next</button>
         </div>
     </main>
 </template>
