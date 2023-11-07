51873fb56836c14add4c419c0f1ee3f4a0379da6
Shaun Simpson <shaungsimpson@gmail.com>
2023-11-07 12:33:04 +1100

    Fix typo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
         <h1>OMDB Movie Search</h1>
         <p>
             This is a small project to demonstrate how to incorporate an API for a simple Vue
-            application taht will allow you to search movies.
+            application that will allow you to search movies.
         </p>
         <p>
             It uses a free API key from OMDB to power the data sets available, and all functionality
